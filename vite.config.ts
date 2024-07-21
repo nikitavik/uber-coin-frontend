5dcaa468b07b0f2e761341869d3a67eaf4a9afc1
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-07-21 18:52:42 +1000

    fix tsconfig. Set vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
-import type { ConfigEnv } from 'vite'
-import { defineConfig, loadEnv } from 'vite'
+import type { ConfigEnv } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 export default ({ mode }: ConfigEnv) => {
-    const currentEnv = loadEnv(mode, process.cwd())
+  const currentEnv = loadEnv(mode, process.cwd());
 
-    return defineConfig({
-        base: currentEnv.VITE_PUBLIC_PATH,
-        mode: mode,
-    })
-}
+  return defineConfig({
+    base: currentEnv.VITE_PUBLIC_PATH,
+    mode: mode,
+    css: {
+      modules: {
+        localsConvention: "camelCase",
+        generateScopedName: `[name]__[local]__[hash:base64:2]`,
+      },
+    },
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@app": "/src/app",
+        "@pages": "/src/pages",
+        "@widgets": "/src/widgets",
+        "@features": "/src/features",
+        "@entities": "/src/entities",
+        "@shared": "/src/shared",
+      },
+    },
+  });
+};
