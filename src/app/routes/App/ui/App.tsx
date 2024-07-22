b5035f17290da04198b8bd02e6b0313de6b1b2b5
Denis <denisp9831@gmail.com>
2024-07-23 09:19:39 +1000

    Button style fix

--- src/app/routes/App/ui/App.tsx
+++ src/app/routes/App/ui/App.tsx
@@ -6,7 +6,12 @@ export const App: FC = () => {
     return (
         <>
             <div className={styles.root}>App</div>
-            <Button text={'Uber'} onClick={() => alert('Click')} />
+            <Button text={'Uber'} onClick={() => alert('Click')} scheme="light" variant="small" />
+            <Button text={'Uber'} onClick={() => alert('Click')} scheme="light" variant="regular" />
+            <Button text={'Uber'} onClick={() => alert('Click')} scheme="light" variant="large" />
+            <Button text={'Uber'} onClick={() => alert('Click')} scheme="dark" variant="small" />
+            <Button text={'Uber'} onClick={() => alert('Click')} scheme="dark" variant="regular" />
+            <Button text={'Uber'} onClick={() => alert('Click')} scheme="dark" variant="large" />
         </>
     );
 };
--- src/shared/button/ui/button.tsx
+++ src/shared/button/ui/button.tsx
@@ -1,14 +1,20 @@
 import React, { FC, HTMLProps } from 'react';
 import { Button as ButtonHeadless } from '@headlessui/react';
+import clsx from 'clsx';
 import styles from './button.module.scss';
 
 type ButtonProps = {
     text: string;
+    scheme: 'light' | 'dark';
+    variant: 'small' | 'regular' | 'large';
 } & React.ComponentPropsWithoutRef<'button'>;
 
-export const Button: FC<ButtonProps> = ({ text, ...props }) => {
+export const Button: FC<ButtonProps> = ({ text, scheme, variant, ...props }) => {
     return (
-        <ButtonHeadless {...props} className={styles.button}>
+        <ButtonHeadless
+            {...props}
+            className={clsx(styles.button, styles[`button_${variant}`], styles[`button_${scheme}`])}
+        >
             {text}
         </ButtonHeadless>
     );
