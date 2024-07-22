c768d4b3e1f0ebbb4a887b0e858098b49db0cc2f
Denis <denisp9831@gmail.com>
2024-07-22 15:59:18 +1000

    quick fix

--- src/app/routes/App/ui/App.tsx
+++ src/app/routes/App/ui/App.tsx
@@ -1,6 +1,12 @@
 import { FC } from 'react';
 import styles from './App.module.scss';
+import { Button } from '@shared/button';
 
 export const App: FC = () => {
-    return <div className={styles.root}>App</div>;
+    return (
+        <>
+            <div className={styles.root}>App</div>;
+            <Button />
+        </>
+    );
 };
