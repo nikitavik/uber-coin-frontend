7b76c7470f08d87a2e741fb47a61d4451ca46231
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-08-10 19:35:55 +1000

    Fix

--- src/widgets/budget-bar/ui/BudgetBar.tsx
+++ src/widgets/budget-bar/ui/BudgetBar.tsx
@@ -1,28 +1,46 @@
-import { FC, useState } from 'react';
+import { FC, useState, useRef, FocusEvent } from 'react';
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
 import { Finance } from '@entities/finance';
 
 import styles from './BudgetBar.module.scss';
 
 export const BudgetBar: FC = () => {
-    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedIndex, setSelectedIndex] = useState<number>(0);
+    const [isOpen, setOpen] = useState<boolean>(false);
+    const localRef = useRef<HTMLDivElement>(null);
+
     const finances = ['Expenses', 'Balance', 'Incomes'];
     const deposits = ['25 975,30 $', '30 975,30 $', '50 975,30 $'];
 
+    const handleOpen = () => setOpen(true);
+    const handleClose = (event: FocusEvent<HTMLDivElement>) => {
+        if (localRef.current && !localRef.current.contains(event.relatedTarget as Node)) {
+            setOpen(false);
+        }
+    };
+
     return (
-        <TabGroup selectedIndex={selectedIndex} onChange={setSelectedIndex}>
-            <TabList className={styles.wrapper}>
+        <TabGroup
+            selectedIndex={selectedIndex}
+            onChange={setSelectedIndex}
+            onFocus={handleOpen}
+            onBlur={handleClose}
+            ref={localRef}
+        >
+            <TabList className={styles.wrapper} ref={localRef}>
                 {finances.map((value, index) => (
-                    <Tab>
+                    <Tab ref={localRef}>
                         <Finance financeType={value} deposit={deposits[index]} />
                     </Tab>
                 ))}
+                {isOpen && (
+                    <TabPanels className={styles.tabPanels}>
+                        <TabPanel className={styles.tabPanel}>Content 1</TabPanel>
+                        <TabPanel className={styles.tabPanel}>Content 2</TabPanel>
+                        <TabPanel className={styles.tabPanel}>Content 3</TabPanel>
+                    </TabPanels>
+                )}
             </TabList>
-            <TabPanels>
-                <TabPanel>Content 1</TabPanel>
-                <TabPanel>Content 2</TabPanel>
-                <TabPanel>Content 3</TabPanel>
-            </TabPanels>
         </TabGroup>
     );
 };
