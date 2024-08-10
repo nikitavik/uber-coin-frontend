7c97ff83c46b9c19079f247de68d850f23dfdaa0
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-08-10 13:42:54 +1000

    Create RootPage and RootPageLayout components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,59 +1,7 @@
-import { FC, useRef } from 'react';
-
-import { Button } from '@shared/button';
-import { TextField, TextFieldPassword } from '@shared/text-field';
-import { AuthModal } from '@widgets/auth-modal';
-
+import { FC } from 'react';
+import { RootPage } from '@pages/root-page';
 import './styles/global.scss';
 
-import styles from './App.module.scss';
-
 export const App: FC = () => {
-    const ref = useRef<HTMLInputElement>(null);
-
-    function handleFocus() {
-        ref.current!.focus();
-    }
-
-    return (
-        <>
-            <div className={styles.root}>App</div>
-            <div className={styles.container}>
-                <form>
-                    <TextField label="label" placeholder="Placeholder" ref={ref} />
-                    <Button onClick={handleFocus}>Uber</Button>
-                </form>
-                <Button onClick={() => alert('rrr')}>Uber</Button>
-                <Button scheme="light">Uber</Button>
-                <Button disabled isFullWidth>
-                    Uber
-                </Button>
-                <Button isLoading>Uber</Button>
-                <Button scheme="light" isLoading>
-                    Uber
-                </Button>
-                <Button isLoading isFullWidth>
-                    Uber
-                </Button>
-                <TextField label="lebel" placeholder="Placeholder" />
-                <TextField
-                    label="label"
-                    placeholder="Placeholder"
-                    hasHintCheck
-                    isCorrect
-                    value="Correct"
-                />
-                <TextField
-                    label="label"
-                    placeholder="Placeholder"
-                    isCorrect={false}
-                    value="Wrong"
-                    hasHintCheck
-                />
-                <TextField label="Label" hintText="Different hint" />
-                <TextFieldPassword label="Password" />
-                <AuthModal />
-            </div>
-        </>
-    );
+    return <RootPage>Root</RootPage>;
 };
---
+++ src/pages/root-page/ui/RootPage.tsx
@@ -0,0 +1,16 @@
+import { ComponentPropsWithoutRef, FC, PropsWithChildren, ReactNode } from 'react';
+import styles from './RootPage.module.scss';
+
+type RootPageProps = {
+    children: ReactNode;
+} & PropsWithChildren<ComponentPropsWithoutRef<'div'>>;
+
+export const RootPage: FC<RootPageProps> = (props) => {
+    const { children, ...restProps } = props;
+
+    return (
+        <div {...restProps} className={styles.app}>
+            {children}
+        </div>
+    );
+};
