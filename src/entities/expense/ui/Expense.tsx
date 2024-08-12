a7836b05fb13880b4df4c608221a2e9c3be6f2a4
Denis <denisp9831@gmail.com>
2024-08-12 16:37:11 +1000

    Create Expense component

---
+++ src/entities/expense/ui/Expense.tsx
@@ -0,0 +1,29 @@
+import { FC, PropsWithChildren, ComponentPropsWithoutRef, ReactNode } from 'react';
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
+import clsx from 'clsx';
+
+import styles from './Expense.module.scss';
+
+type ExpenseProps = {
+    children: ReactNode;
+    expenseName: string;
+    deposit: string;
+    className?: string;
+} & PropsWithChildren<ComponentPropsWithoutRef<'div'>>;
+
+export const Expense: FC<ExpenseProps> = (props) => {
+    const { children, expenseName, deposit, className, ...restProps } = props;
+
+    return (
+        <div className={clsx(styles.wrapper, className)} {...restProps}>
+            <Popover className={styles.expense}>
+                <span className={styles.expenseName}>{expenseName}</span>
+                <PopoverButton className={styles.icon}>{children}</PopoverButton>
+                <span className={styles.deposit}>{deposit}</span>
+                <PopoverPanel transition anchor="bottom" className={styles.popoverPanel}>
+                    Info about expense
+                </PopoverPanel>
+            </Popover>
+        </div>
+    );
+};
--- src/pages/root-page/root-page-layout/ui/RootPageLayout.tsx
+++ src/pages/root-page/root-page-layout/ui/RootPageLayout.tsx
@@ -2,6 +2,9 @@ import { ComponentPropsWithoutRef, FC, PropsWithChildren, ReactNode } from 'reac
 import { BudgetBar } from '@widgets/budget-bar';
 import { AccountBar } from '@widgets/accounts-bar';
 import { Sidebar } from '@shared/sidebar';
+// TODO убрать expense и icon
+import { Expense } from '@entities/expense';
+import Icon from '@entities/account/assets/icons/credit-card/credit_card.svg?react';
 import clsx from 'clsx';
 
 import styles from './RootPageLayout.module.scss';
@@ -19,6 +22,42 @@ export const RootPageLayout: FC<RootPageLayoutProps> = (props) => {
             <main className={styles.main}>
                 <BudgetBar className={styles.budget} />
                 <AccountBar className={styles.account} />
+                {/* //TODO сделать компонент */}
+                <div className={styles.expanses}>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                    <Expense expenseName="Вкусно и точка" deposit="10 000 $">
+                        {<Icon />}
+                    </Expense>
+                </div>
             </main>
             <Sidebar className={clsx(styles.sidebarLayout, styles.sidebarRight)} />
             {children}
