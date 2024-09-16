bbba689fbf14f11e9025c17898dd2c0066bf1170
Denis <denisp9831@gmail.com>
2024-09-16 12:48:14 +1000

    Quick fix

--- src/widgets/operations-history-list/ui/OperationsHistoryList.tsx
+++ src/widgets/operations-history-list/ui/OperationsHistoryList.tsx
@@ -8,10 +8,36 @@ import styles from './OperationsHistoryList.module.scss';
 export const OperationsHistoryList: FC = () => {
     return (
         <div className={styles.operationsHistoryList}>
-            <h1>Operations list</h1>
+            <h3>Operations list</h3>
             <div className={styles.operationsBlock}>
                 <div className={styles.operationsHeader}>
                     <span className={styles.operationsDate}>Yesterday</span>
+                    <span className={styles.balance}>1000</span>
+                </div>
+                <div className={styles.operaions}>
+                    <Operation
+                        icon={<CreditCard />}
+                        currency="KGS"
+                        operationCategory="Food"
+                        balance="1000"
+                    />
+                    <Operation
+                        icon={<CreditCard />}
+                        currency="KGS"
+                        operationCategory="Food"
+                        balance="1000"
+                    />
+                    <Operation
+                        icon={<CreditCard />}
+                        currency="KGS"
+                        operationCategory="Food"
+                        balance="1000"
+                    />
+                </div>
+            </div>
+            <div className={styles.operationsBlock}>
+                <div className={styles.operationsHeader}>
+                    <span className={styles.operationsDate}>20.03.2121</span>
                     <span className={styles.balance}>1000</span>
                 </div>
                 <div className={styles.operaions}>
