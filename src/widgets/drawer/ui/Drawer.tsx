5f7af6d62c9083b57e082112fbaaf4e33c870dc9
Nikita Kornilov <krnl@feip.co>
2024-09-17 22:59:36 +0500

    Rework drawer

--- src/widgets/drawer/ui/Drawer.tsx
+++ src/widgets/drawer/ui/Drawer.tsx
@@ -1,28 +1,41 @@
 import { FC, PropsWithChildren, useState } from 'react';
 
-import { Modal } from '@shared/modal';
+import clsx from 'clsx';
 
 import MenuIcon from '../assets/menu.svg?react';
 import OpenMenuIcon from '../assets/menu_open.svg?react';
 import styles from './Drawer.module.scss';
 
-export const Drawer: FC<PropsWithChildren> = (props) => {
-    const { children } = props;
+export const Drawer: FC<PropsWithChildren> = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     return (
         <>
-            <button type="button" onClick={() => setIsOpen(true)} className={styles.button}>
-                {isOpen ? (
-                    <MenuIcon className={styles.icon} />
-                ) : (
-                    <OpenMenuIcon className={styles.icon} />
-                )}
-            </button>
+            <div
+                onClick={() => setIsOpen(false)}
+                className={clsx(styles.backdrop, isOpen && styles.isVisible)}
+            />
 
-            <Modal open={isOpen} onClose={setIsOpen} className={styles.drawerModal} transition>
-                {children}
-            </Modal>
+            <div className={clsx(styles.root, isOpen && styles.isOpen)}>
+                <div className={styles.inner}>
+                    <button
+                        type="button"
+                        onClick={() => setIsOpen((prev) => !prev)}
+                        className={styles.button}
+                    >
+                        {isOpen ? (
+                            <MenuIcon className={styles.icon} />
+                        ) : (
+                            <OpenMenuIcon className={styles.icon} />
+                        )}
+                    </button>
+
+                    <div className={styles.content}>Content</div>
+                    {/*<Modal open={isOpen} onClose={setIsOpen} className={styles.drawerModal} transition>*/}
+                    {/*    {children}*/}
+                    {/*</Modal>*/}
+                </div>
+            </div>
         </>
     );
 };
